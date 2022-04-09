bfefef73527fe7ba1c09b1af1abeabf7f0b7f7f0
ankita <ankita.khangar@parallel-minds.in>
2022-04-09 13:57:34 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NoteComponent } from './components/note/note.component';
+import { CardComponent } from './components/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NoteComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
